4dbbb18f92f9efc8abf34292c6bfce4d3074966a
duan <duan@192.168.54.117>
2020-03-12 13:37:38 +0800

    update

--- merge_use_small_dtype.py
+++ merge_use_small_dtype.py
@@ -22,14 +22,14 @@ def preprocess():
 def main(mode,phase=2):
     ''' mode = 'train' or 'test'
     '''
-    print 'merge process ...... '+mode
+    print('merge process ...... '+mode)
     mode_csv = './datasets/train/' + mode + '.csv'
     #********************************
     if mode =='train':
         mode_csv = preprocess()
     #********************************
     df = pd.read_csv(mode_csv).apply(small_dtype)
-    print 'process conversiontime'
+    print('process conversiontime')
     if 'conversionTime' in df.columns:
         df['conversionTime'] = df['conversionTime'].fillna(0).astype('int32')
     if int(phase) == 1:
@@ -41,11 +41,11 @@ def main(mode,phase=2):
     df = pd.merge(df, pd.read_csv('./datasets/train/user.csv').apply(small_dtype),'left',on='userID')
     df = pd.merge(df, pd.read_csv('./datasets/train/position.csv').apply(small_dtype),'left',on='positionID')
     df = pd.merge(df, pd.read_csv('./datasets/train/app_categories.csv').apply(small_dtype),'left',on='appID')
-    print df.info()
+    print(df.info())
     df = pd.merge(df, pd.read_csv('./datasets/train/tb_user_app_actions_add_one_column.csv',dtype='int32').apply(small_dtype),'left',on=['userID','appID'])
     df.installTime = df.installTime.fillna(99999999).astype('int32')
     df.install_day = df.install_day.fillna(99).astype('int8')
-    print df.info()
+    print(df.info())
     df['idx'] = df.index.astype('int32')
     df.to_csv('./datasets/train/' + mode + '_ad_user_position_appCates_act.csv',index=False, chunksize=100000)
 
